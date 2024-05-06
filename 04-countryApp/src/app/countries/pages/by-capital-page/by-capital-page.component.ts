fabf9d6bd34662c6139d71d32e048326e0da3e66
Lucia Rico <lrico@penalara.com>
2024-05-06 13:15:29 +0200

    Nuevos cambios hasta el vídeo 126

--- 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -9,9 +11,16 @@ import { Component, ViewChild } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  public countries: Country[] = [];
+
+  constructor( private countriesService: CountriesService ) {}
+
   searchByCapital( term: string ): void {
-    console.log('Desde ByCapitalPage');
-    console.log({ term });
+    this.countriesService.searchCapital( term )
+    //necesito el subscribe para recibir notificaciones
+      .subscribe( countries =>  {
+          this.countries = countries;
+        });
   }
 
 }
