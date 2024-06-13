9585186c6e2239330cbc8a8d2b3561f775688d3d
Lucia Rico <lrico@penalara.com>
2024-06-13 14:01:45 +0200

    Empiezo sección 14 sobre Material

--- 06-heroesApp/src/app/auth/auth.module.ts
+++ 06-heroesApp/src/app/auth/auth.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { LayoutPageComponent } from './pages/layoutPage/layoutPage.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
 
 
 
@@ -17,6 +20,7 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MaterialModule,
   ]
 })
 export class AuthModule { }
---
+++ 06-heroesApp/src/environments/enviroments.prod.ts
@@ -0,0 +1,4 @@
+
+export const enviroments = {
+  baseUrl: 'http://lucia-penalara.com/api'
+}
