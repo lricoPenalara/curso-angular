5d66146e6c38184864e3f6d5a40278dc7e5cde85
Lucia Rico <lrico@penalara.com>
2024-06-14 13:54:47 +0200

    Completado hasta el vídeo 201 de la sección 14 de héroes

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -9,6 +9,8 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 
 
@@ -19,6 +21,10 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     ListPageComponent,
     NewPageComponent,
     SearchPageComponent,
+    CardComponent,
+
+    // Pipes
+    HeroImagePipe,
   ],
   imports: [
     CommonModule,
--- 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
   styles: ``
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+
+  // Puede ser opcinal porque cuando un componente se monta
+  // puede ser nulo en ese momento
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.heroesService.getHeroById( id ) ),
+      )
+      .subscribe( hero => {
+
+        if( !hero ) return this.router.navigate([ '/heroes/list' ])
+
+        this.hero = hero;
+        return;
+      })
+  }
+
+  goBack():void{
+    this.router.navigateByUrl('heroes/list')
+  }
 
 }
