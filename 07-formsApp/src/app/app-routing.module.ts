895431e813491e0269abe7778d545d3ddf27522a
Lucia Rico <lrico@penalara.com>
2024-07-04 14:31:12 +0200

    Cambios en el proyecto 07-formsApp. Añadido formulario reactivo en basic-page

--- 07-formsApp/src/app/app-routing.module.ts
+++ 07-formsApp/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Acceder a todos los formularios reactivos
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule),
+  },
+  {
+    path: '**', //ruta por defecto si no pongo nada en la ruta
+    redirectTo: 'reactive',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+    name: 'RTX 5090',
+    price: 2500,
+    inStorage: 6,
+}
+
+@Component({
+    templateUrl: './basic-page.component.html',
+    styles: ``
+})
+export class BasicPageComponent implements OnInit {
+
+//Las dos formas son válidas
+
+    // public myForm: FormGroup = new FormGroup({
+    //     name: new FormControl(''),
+    //     price: new FormControl(0),
+    //     inStorage: new FormControl(0),
+    // });
+
+    public myForm: FormGroup = this.fb.group({
+        name: ['', [ Validators.required, Validators.minLength(3) ] ],
+        price: [0, [ Validators.required, Validators.min(0) ] ],
+        inStorage: [0, [ Validators.required, Validators.min(0) ] ],
+    })
+
+    constructor( private fb: FormBuilder ){}
+
+    ngOnInit(): void {
+        // this.myForm.reset(rtx5090);
+    }
+
+    // Comprueba si en el campo hay errores cuando toca
+    isNotValidField( field: string ): boolean | null {
+        return this.myForm.controls[field].errors
+            && this.myForm.controls[field].touched;
+    }
+
+    getFieldError( field: string ): string | null {
+
+        // Si el form no tiene ese campo no regreso nada
+        if ( !this.myForm.controls[field] ) return null;
+
+        // Si el form si tiene el campo
+        // || {} => Si errors es nulo regresa un objeto vacío
+        const errors = this.myForm.controls[field].errors || {};
+
+        for ( const key of Object.keys(errors) ) {
+            switch( key ) {
+                case 'required':
+                return 'Este caso es requerido';
+
+                case 'minlength':
+                return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;
+            }
+
+        }
+
+        return null;
+
+    }
+
+    onSave():void {
+
+        if ( this.myForm.invalid ) {
+
+            //marca todos los campos como si fuesen tocados
+            this.myForm.markAllAsTouched();
+            return;
+        }
+
+        console.log(this.myForm.value);
+
+        this.myForm.reset({ price: 10, inStorage: 0 });
+    }
+
+}
