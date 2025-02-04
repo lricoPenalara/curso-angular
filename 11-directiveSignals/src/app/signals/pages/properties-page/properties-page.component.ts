d55d1176e2e80b5c75544386b1a7880813e79902
lricoPenalara <lrico@penalara.com>
2025-02-04 17:41:57 +0100

    Cambios seccion 11

---
+++ 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-properties-page',
+  templateUrl: './properties-page.component.html',
+  styles: ``
+})
+export class PropertiesPageComponent {
+
+}
