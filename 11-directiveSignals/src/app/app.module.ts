912d3dbe85428a895f2614ac9242799ec623fdd3
lricoPenalara <lrico@penalara.com>
2025-02-05 17:11:50 +0100

    Fin seccion 24, proyecto 11

--- 11-directiveSignals/src/app/app.module.ts
+++ 11-directiveSignals/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,10 +12,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration(),
-    provideHttpClient(),
   ],
   bootstrap: [AppComponent]
 })
--- 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { UsersServiceService } from '../../services/users-service.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -14,6 +14,11 @@ export class UserInfoPageComponent implements OnInit {
 
   public currentUser = signal<User|undefined>(undefined); //puede tener dos valores
   public userWasFound = signal(true);
+  public fullName = computed<string>( () => {
+    if ( !this.currentUser() ) return 'Usuario no encontrado';
+
+    return `${ this.currentUser()?.first_name } ${ this.currentUser()?.last_name }`;
+  });
 
   ngOnInit(): void {
     this.loadUser( this.userId() );
@@ -23,11 +28,19 @@ export class UserInfoPageComponent implements OnInit {
     if (id <= 0 ) return;
 
     this.userId.set(id);
+    this.currentUser.set(undefined);
 
     this.userService.getUserById( id )
-      .subscribe( user => {
-        this.currentUser.set( user )
-      })
+      .subscribe({
+        next: (user) => {
+            this.currentUser.set( user );
+            this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false),
+          this.currentUser.set(undefined)
+        }
+      });
 
   }
 }
