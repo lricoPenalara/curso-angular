ea74e4e883809240badbb787793dddacdc28e415
lricoPenalara <lrico@penalara.com>
2025-02-07 14:11:59 +0100

    Nuevos cambios seccion material

--- 12-angular-my-dashboard/src/app/dashboard/pages/material/material.component.ts
+++ 12-angular-my-dashboard/src/app/dashboard/pages/material/material.component.ts
@@ -1,10 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+
 
 @Component({
   selector: 'app-material',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './material.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
