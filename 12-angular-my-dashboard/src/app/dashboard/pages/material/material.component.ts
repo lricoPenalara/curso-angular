fe41802d8ef516d6b08a9cfbc70633ea1bced0cb
lricoPenalara <lrico@penalara.com>
2025-02-07 13:26:56 +0100

    Guardar cambios proyecto

---
+++ 12-angular-my-dashboard/src/app/dashboard/pages/material/material.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-material',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './material.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class MaterialComponent {
+
+}
---
+++ 12-angular-my-dashboard/src/app/dashboard/pages/ui/product-card/product-card.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, ChangeDetectionStrategy, Input, input, Output, EventEmitter, output, effect } from '@angular/core';
+import { Product } from '@interfaces/product.interface';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-card.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductCardComponent {
+
+  // @Input({ required: true, })
+  //  public product!: Product;
+  public product = input.required<Product>();
+
+  // @Output()
+  // public onIncrementQuantity = new EventEmitter<number>();
+  public onIncrementQuantity = output<number>();
+
+  public incrementQuantity(): void {
+    this.onIncrementQuantity.emit(this.product().quantity + 1);
+  }
+
+  public loginEffect = effect(() => {
+    console.log(this.product().name);
+  })
+
+
+}
